01f208a411b86f2566961b151ff1b0cf1be7d019
pablosg789 <42720199+pablosg789@users.noreply.github.com>
2019-03-21 11:27:08 -0300

    Add files via upload

---
+++ POO/app.py
@@ -0,0 +1,306 @@
+#!/usr/bin/env python
+import operator
+import csv
+from datetime import datetime
+from flask import Flask, render_template, redirect, url_for, flash, session, request
+from flask_bootstrap import Bootstrap
+# from flask_moment import Moment
+from flask_script import Manager
+from forms import LoginForm, SaludarForm, RegistrarForm
+from mis_clases import Validacion, Archivo, Utilidad
+from os import remove
+
+app = Flask(__name__)
+manager = Manager(app)
+bootstrap = Bootstrap(app)
+# moment = Moment(app)
+
+app.config['SECRET_KEY'] = 'un string que funcione como llave'
+
+
+@app.route('/')
+def index():
+    return render_template('index.html',nombre='Inicio', fecha_actual=datetime.utcnow())
+
+
+@app.route('/saludar', methods=['GET', 'POST'])
+def saludar():
+    formulario = SaludarForm()
+    if formulario.validate_on_submit():
+        print(formulario.usuario.name)
+        return redirect(url_for('saludar_persona', usuario=formulario.usuario.data))
+    return render_template('saludar.html', form=formulario)
+
+
+@app.route('/saludar/<usuario>')
+def saludar_persona(usuario):
+    return render_template('usuarios.html', nombre=usuario)
+
+
+@app.errorhandler(404)
+def no_encontrado(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def error_interno(e):
+    return render_template('500.html'), 500
+
+
+@app.route('/ingresar', methods=['GET', 'POST'])
+def ingresar():
+    formulario = LoginForm()
+    if formulario.validate_on_submit():
+        with open('usuarios') as archivo:
+            usuarios_csv = csv.reader(archivo)
+            registro = next(usuarios_csv)
+            while registro:
+                if formulario.usuario.data == registro[0] and formulario.password.data == registro[1]:
+                    flash('Bienvenido')
+                    session['username'] = formulario.usuario.data
+                    confirmacion = open("usuario_confirmado","w")
+                    confirmacion.close()
+                # Ingreso de usuario
+
+                # Ingresar a TABLA DE VENTAS
+                    if Validacion.ValidarArchivo('tabla.csv'):
+                        archivo_csv = Archivo.AbrirComoLista('tabla.csv')
+                        cliente = []
+                        codigo = []
+                        producto = []
+                        cantidad = []
+                        precio = []
+                        for lista in archivo_csv:
+                            codigo.append(lista[0])
+                            producto.append(lista[1])
+                            cliente.append(lista[2])
+                            cantidad.append(lista[3])
+                            precio.append(lista[4])
+                        rango = len(codigo)
+                        # Validacion de campos
+                        if Validacion.ValidarCampoCodigo(codigo[1:]) == False:
+                            return render_template('no_valido.html' , mensaje = 'El campo CODIGO debe estar compuesto por 3 letras máyusculas seguidas de 3 caracteres númericos')
+                        elif Validacion.ValidarCampoCantidad(cantidad[1:]) == False:
+                            return render_template('no_valido.html' , mensaje = 'El campo CANTIDAD solo permite números enteros')
+                        elif Validacion.ValidarCampoPrecio(precio[1:]) == False:
+                            return render_template('no_valido.html' , mensaje = 'El campo PRECIO solo premite números decimales')
+                        elif Validacion.ValidarCampos(archivo_csv) == False:
+                            return render_template('no_valido.html' , mensaje= 'Verifique la cantidad de CAMPOS ingresados  en su archivo CSV')
+                        else:
+                            return render_template('tabla.html',nombre= 'Tabla de Ventas',nameParametro = 'TABLA', campo1 = codigo, campo2 = producto,campo3 = cliente,
+                            campo4 = cantidad, campo5 = precio, rango= rango)
+                    else:
+                        return render_template('no_valido.html', mensaje= 'El csv archivo inexistente')
+                registro = next(usuarios_csv, None)
+            else:
+                flash('Revisá nombre de usuario y contraseña')
+                return redirect(url_for('ingresar'))
+    return render_template('login.html', formulario=formulario)
+
+@app.route('/compra_cliente', methods=["GET", 'POST'])
+def compra_cliente():
+    if request.method == 'POST':
+        nombre_cliente = request.form['nombre']
+        apellido_cliente = request.form['apellido']
+        nombre_completo = Utilidad.CombinarNombre(nombre_cliente,apellido_cliente)
+        if nombre_completo == False:
+            return render_template('no_valido.html', mensaje= 'Error al ingresar los datos del cliente')
+        archivo_csv = Archivo.AbrirComoLista('tabla.csv')
+# crea un diccionario que contiene en listas el contenido de los 5 campos
+        dic_campos = Archivo.DicCampos(archivo_csv)
+        if nombre_completo in dic_campos['cliente']:
+# se crea un diccionario con 2 que contiene 2 listas una con los nombres de los productos y otra con los codigos
+            cliente_1 = Utilidad.CompradoPorCliente(nombre_completo,dic_campos['cliente'],dic_campos['codigo'],dic_campos['producto'])
+            return render_template('compra_cliente.html', nombre_completo = nombre_completo ,codigo = cliente_1['codigo']
+            ,producto = cliente_1['producto'] ,rango = len(cliente_1['codigo']))
+        else:
+            return render_template('no_valido.html', mensaje='No se registran clientes con el nombre {}'.format(nombre_completo))
+    return render_template('sin_permiso.html')
+
+@app.route('/clientes_por_producto', methods=["GET", 'POST'])
+def clientes_por_producto():
+    if request.method == 'POST':
+        codigo_producto = request.form['codigo']
+        codigo_producto = codigo_producto.upper()
+        if codigo_producto == False:
+            return render_template('no_valido.html', mensaje= 'Error en el código del producto')
+        archivo_csv = Archivo.AbrirComoLista('tabla.csv')
+# crea un diccionario que contiene en listas el contenido de los 5 campos
+        dic_campos = Archivo.DicCampos(archivo_csv)
+        if codigo_producto in dic_campos['codigo']:
+# se crea un diccionario con 1 string con el nombre del producto y una lista con los nombres de los clientes
+            dicc = Utilidad.ClientesPorProducto(codigo_producto,dic_campos['codigo'],dic_campos['cliente'],dic_campos['producto'])
+            nombre_producto = dicc['producto']
+            lista_clientes = dicc['clientes']
+            return render_template('clientes_por_producto.html', codigo_producto = codigo_producto , nombre_producto = nombre_producto
+            ,lista_clientes = lista_clientes ,rango = len(lista_clientes))
+        else:
+            return render_template('no_valido.html', mensaje='El código {} no se encuentra registrado'.format(codigo_producto))
+    return render_template('sin_permiso.html')
+
+@app.route('/mejores_clientes',methods=["GET", 'POST'])
+def mejores_clientes():
+    if request.method == 'POST':
+        archivo_csv = Archivo.AbrirComoLista('tabla.csv')
+    # crea un diccionario que contiene en listas el contenido de los 5 campos
+        dic_campos = Archivo.DicCampos(archivo_csv)
+    # se crea un diccionario con 1 string con el nombre del producto y una lista con los nombres de los clientes
+        cliente_gasto = Utilidad.TopClientes(dic_campos['cliente'],archivo_csv)
+    # se ordenan las listas de mayor a menor segun los gastos de los clientes
+        dic_cliente_gasto = Utilidad.OrdenarListasClienteGastos(cliente_gasto[0],cliente_gasto[1])
+        rango = len(cliente_gasto[0])
+        return render_template('mejores_clientes.html',lista_clientes = dic_cliente_gasto['cliente'], lista_gastos = dic_cliente_gasto['gasto'], rango = rango)
+    else:
+        return render_template('sin_permiso.html')
+
+@app.route('/productos_mas_vendidos',methods=["GET", 'POST'])
+def productos_mas_vendidos():
+    if request.method == 'POST':
+        archivo_csv = Archivo.AbrirComoLista('tabla.csv')
+    # crea un diccionario que contiene en listas el contenido de los 5 campos
+        dic_campos = Archivo.DicCampos(archivo_csv)
+    # se crea un diccionario con 1 string con el nombre del producto y una lista con los nombres de los clientes
+        producto_cantidad = Utilidad.TopProductos(dic_campos['producto'],archivo_csv)
+    # se ordenan las listas de mayor a menor segun la cantidad de ventas
+        dic_prod_cantidad = Utilidad.OrdenarListasProductosCantidad(producto_cantidad[0],producto_cantidad[1])
+        rango = len(producto_cantidad[0])
+        return render_template('productos_mas_vendidos.html',lista_productos = dic_prod_cantidad['producto'],
+         lista_cantidad = dic_prod_cantidad['cantidad'], rango = rango)
+    else:
+        return render_template('sin_permiso.html')
+
+
+
+@app.route('/registrar', methods=['GET', 'POST'])
+def registrar():
+    formulario = RegistrarForm()
+    if formulario.validate_on_submit():
+        if formulario.password.data == formulario.password_check.data:
+            with open('usuarios', 'a+', newline='') as archivo:
+                archivo_csv = csv.writer(archivo)
+                registro = [formulario.usuario.data, formulario.password.data]
+                archivo_csv.writerow(registro)
+            flash('Usuario creado correctamente')
+            return redirect(url_for('ingresar'))
+        else:
+            flash('Las passwords no matchean')
+    return render_template('registrar.html', form=formulario)
+
+@app.route('/carga_de_ventas')
+def carga_de_ventas():
+    if Validacion.ValidarArchivo('usuario_confirmado'):
+        return render_template('carga_de_ventas.html')
+        
+    else:
+        return render_template('sin_permiso.html')
+
+@app.route('/cerrar_sesion')
+def cerrar_sesion():
+    if Validacion.ValidarArchivo('usuario_confirmado'):
+        remove('usuario_confirmado')
+        return redirect(url_for('index'))
+    else:
+        return render_template('login.html', formulario=formulario)
+        
+
+@app.route('/tabla')
+def tabla():
+    if Validacion.ValidarArchivo('usuario_confirmado'):
+        if Validacion.ValidarArchivo('tabla.csv'):
+            archivo_csv2 = Archivo.AbrirComoLista('tabla.csv')
+            cliente = []
+            codigo = []
+            producto = []
+            cantidad = []
+            precio = []
+            for lista in archivo_csv2:
+                codigo.append(lista[0])
+                producto.append(lista[1])
+                cliente.append(lista[2])
+                cantidad.append(lista[3])
+                precio.append(lista[4])
+            rango = len(codigo)
+            return render_template('tabla.html',nombre= 'Tabla de Ventas',nameParametro = 'TABLA', campo1 = codigo, campo2 = producto,campo3 = cliente,
+                                campo4 = cantidad, campo5 = precio, rango= rango)
+        else:
+            return render_template('no_valido.html', mensaje= 'El csv archivo inexistente')
+    else:
+        return render_template('sin_permiso.html')
+
+@app.route('/ultimas_ventas')
+def ultimas_ventas():
+    if Validacion.ValidarArchivo('usuario_confirmado'):
+        if Validacion.ValidarArchivo('tabla.csv'):
+            archivo_csv2 = Archivo.AbrirComoLista('tabla.csv')
+            cliente = []
+            codigo = []
+            producto = []
+            cantidad = []
+            precio = []
+            for lista in archivo_csv2:
+                codigo.append(lista[0])
+                producto.append(lista[1])
+                cliente.append(lista[2])
+                cantidad.append(lista[3])
+                precio.append(lista[4])
+            rango = len(codigo)
+            return render_template('ultimas_ventas.html',nombre= 'Ultimas Ventas',nameParametro = 'TABLA', campo1 = codigo, campo2 = producto,campo3 = cliente,
+                                campo4 = cantidad, campo5 = precio, rango= rango)
+        else:
+            return render_template('no_valido.html', mensaje= 'El csv archivo inexistente')
+    else:
+        return render_template('sin permiso.html')
+
+@app.route('/agrega_venta', methods=['GET','POST'])
+def agrega_venta():
+    if request.method == 'POST':
+        if Validacion.ValidarArchivo('usuario_confirmado'):
+            nombre_cliente = request.form['nombre']
+            apellido_cliente = request.form['apellido']
+            nombre_completo = Utilidad.CombinarNombre(nombre_cliente,apellido_cliente)
+            codigo_producto = request.form['codigo']
+            nombre_producto = request.form['nombre_producto']
+            nuevo_precio = request.form['precio']
+            nuevo_cantidad = request.form['cantidad']
+            if Validacion.ValidarCampoCodigo2(codigo_producto) == False:
+                return render_template('no_valido.html' , mensaje = 'El campo CODIGO debe estar compuesto por 3 letras máyusculas seguidas de 3 caracteres númericos')
+            elif Validacion.ValidarCampoCantidad2(nuevo_cantidad) == False:
+                return render_template('no_valido.html' , mensaje = 'El campo CANTIDAD solo permite números enteros')
+            elif Validacion.ValidarCampoPrecio2(nuevo_precio) == False:
+                return render_template('no_valido.html' , mensaje = 'El campo PRECIO solo premite números decimales')
+            else:
+                with open('tabla.csv','a', newline='') as archivo_csv:
+                    escribir_csv = csv.writer(archivo_csv)
+                    nueva_venta = [codigo_producto,nombre_producto,nombre_completo,nuevo_cantidad,nuevo_precio]
+                    escribir_csv.writerow(nueva_venta)
+                return redirect(url_for('ultimas_ventas'))
+        else:
+            return render_template('sin_permiso.html')
+    else:
+        return render_template('sin_permiso.html')
+    
+    
+    
+
+
+
+@app.route('/secret', methods=['GET'])
+def secreto():
+    if 'username' in session:
+        return render_template('private.html', username=session['username'])
+    else:
+        return render_template('sin_permiso.html')
+
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    if 'username' in session:
+        session.pop('username')
+        return render_template('logged_out.html')
+    else:
+        return redirect(url_for('index'))
+
+
+if __name__ == "__main__":
+    #app.run(host='0.0.0.0', debug=True)
+    manager.run()
---
+++ POO/mis_clases.py
@@ -0,0 +1,263 @@
+import operator
+import csv
+import string
+class Validacion():
+
+
+    def ValidarArchivo(archivo):
+        try:
+            with open(archivo) as archivo_csv:
+                return True
+
+        except FileNotFoundError:
+            return False
+
+    def ValidarCampos(lista):
+        try:
+            for i in lista:
+                if len(i) == 5:
+                    condicion = True
+                else:
+                    condicion = False
+                    if condicion == False:
+                        break
+            return condicion
+        except:
+            return False
+
+    def ValidarCampoCodigo(lista):
+        try:
+            for valor in lista:
+                if valor[0:3].isupper() and valor[0:3].isalpha() and valor.isalnum() and valor[3:6].isdigit() and len(valor) == 6:
+                    condicion = True
+                else:
+                    condicion = False
+                    if condicion == False:
+                        break
+            return condicion
+        except:
+            return False
+    
+    def ValidarCampoCodigo2(codigo):
+        try:
+            if codigo[0:3].isupper() and codigo[0:3].isalpha() and codigo.isalnum() and codigo[3:6].isdigit() and len(codigo) == 6:
+                return True
+            else:
+                return False
+        except:
+            return False
+
+    def ValidarCampoCantidad(lista):
+            try:
+                for valor in lista:
+                    if type(valor) == str:
+                        numero = eval(valor)
+                        if numero % 1 == 0:
+                            condicion = True
+                        else:
+                            condicion = False
+                            if condicion == False:
+                                break
+                    elif valor % 1 == 0:
+                        condicion = True
+                    else:
+                        condicion = False
+                        if condicion == False:
+                            break
+                return condicion
+            except:
+                return False
+    def ValidarCampoCantidad2(cantidad):
+        try:
+            if type(cantidad) == str:
+                numero = eval(cantidad)
+                if numero % 1 == 0:
+                    return True
+                else:
+                    return False
+        except:
+            return False            
+
+    def ValidarCampoPrecio(lista):
+        try:
+            for valor in lista:
+                if float(valor) % 1 != 0:
+                    condicion = True
+                else:
+                    condicion = False
+                    if condicion == False:
+                        break
+            return condicion
+        except:
+            return False
+    
+    def ValidarCampoPrecio2(precio):
+        try:
+            if float(precio) % 1 != 0:
+                return True
+            else:
+                return False
+        except:
+            return False
+    
+
+class Archivo():
+
+    def AbrirComoLista(archivo):
+        with open(archivo) as csv_file:
+            leer_archivo = csv.reader(csv_file)
+            campos = []
+            for campo_lista in leer_archivo:
+                campos.append(campo_lista)
+            return campos
+    def DicCampos(campos):
+        cliente = []
+        codigo = []
+        producto = []
+        cantidad = []
+        precio = []
+        for lista in campos:
+            codigo.append(lista[0])
+            producto.append(lista[1])
+            cliente.append(lista[2])
+            cantidad.append(lista[3])
+            precio.append(lista[4])
+        dic_campos = {}
+        dic_campos['cliente']= cliente
+        dic_campos['codigo']= codigo
+        dic_campos['producto']= producto
+        dic_campos['cantidad']= cantidad
+        dic_campos['precio']= precio
+        return  dic_campos
+    
+
+class Utilidad():
+
+    def CombinarNombre(nombre,apellido): # combina nombre y apellido capitalizados
+        try:
+            nombre_completo = nombre.strip().capitalize() +' '+ apellido.strip().capitalize()
+            return nombre_completo
+        except:
+            return False
+
+    def OrdenarDiccionario(diccionario):
+        try:
+            diccionario = sorted(diccionario.items(),key=operator.itemgetter(1))
+            return diccionario
+
+        except:
+            return False
+
+    def CompradoPorCliente(nombre,cliente,codigo,producto):
+        try:
+            indice= []
+            for contador, consulta in enumerate(cliente):
+                if consulta == nombre:
+                    indice.append(contador)
+            cod = []
+            pro = []
+            for i in indice:
+                cod.append(codigo[i])
+                pro.append(producto[i])
+            productos_comprados_por_cliente = {}
+            productos_comprados_por_cliente['producto'] = pro
+            productos_comprados_por_cliente['codigo']= cod
+            return productos_comprados_por_cliente
+
+        except:
+            return False
+
+    def ClientesPorProducto(num_cod,codigo,cliente,producto):
+        try:
+            indice= []
+            for contador, consulta in enumerate(codigo):
+                if consulta == num_cod:
+                    indice.append(contador)
+            nombre_producto = producto[indice[0]]
+            nombres = []
+            for i in indice:
+                nombres.append(cliente[i])
+            producto_con_lista_clientes = {}
+            producto_con_lista_clientes['clientes']= nombres
+            producto_con_lista_clientes['producto']= nombre_producto
+            return producto_con_lista_clientes
+        except:
+            return False
+
+    def TopClientes(cliente,archivo_csv):
+        try:
+            list_nombre = []
+            list_gasto = []
+            for nombre in cliente: # para nombre el nombre de la lista cliente
+                if nombre in list_nombre:
+                    continue
+                for i in archivo_csv[1:]: # para i la lista de campos
+
+                    if nombre in i: # si nombre esta en i(la lista de campos)
+                        if nombre in list_nombre: # si nombre(uno de la lista de clientes) esta en list_nombre
+                            indice = list_nombre.index(nombre) # indice guarda el numero de posicion
+                            list_gasto[indice] = list_gasto[indice] + float(i[3]) * float(i[4]) # cambia el valor que ya tenia list_gasto
+
+                        else: # si no esta nombre en i (la lista de campos)
+                            list_nombre.append(nombre) # se debe agregar nombre a list_nombre
+                            list_gasto.append(float(i[3]) * float(i[4])) # se debe agregar los gastos a list_gasto
+            cliente_gasto = [list_nombre,list_gasto]
+            return cliente_gasto
+        except:
+            return False
+
+    def TopProductos(prducto,archivo_csv):
+        try:
+            list_producto = []
+            list_cantidad = []
+            for nombre in prducto: # para nombre el nombre de la lista cliente
+                if nombre in list_producto:
+                    continue
+                for i in archivo_csv[1:]: # para i la lista de campos
+                    if nombre in i: # si nombre esta en i(la lista de campos)
+                        if nombre in list_producto: # si nombre(uno de la lista de clientes) esta en list_producto
+                            indice = list_producto.index(nombre) # indice guarda el numero de posicion
+                            list_cantidad[indice] = list_cantidad[indice] + int(i[3]) # cambia el valor que ya tenia list_cantidad
+                        else: # si no esta nombre en i (la lista de campos)
+                            list_producto.append(nombre) # se debe agregar nombre a list_producto
+                            list_cantidad.append(int(i[3])) # se debe agregar los gastos a list_cantidad
+            producto_cantidad = [list_producto,list_cantidad]
+            return producto_cantidad
+        except:
+            return False
+
+    def OrdenarListasProductosCantidad(producto,cantidad):
+        try:
+            for i in range(len(cantidad)):
+                for x in range(len(cantidad)-1):
+                    if int(cantidad[x]) < int(cantidad[x+1]):
+                        aux1= int(cantidad[x])
+                        cantidad[x] = int(cantidad[x+1])
+                        cantidad[x+1]= aux1
+                        aux2=producto[x]
+                        producto[x]=producto[x+1]
+                        producto[x+1]=aux2
+            dic = {}
+            dic['producto'] = producto
+            dic['cantidad'] = cantidad
+            return dic
+        except:
+            return False
+
+    def OrdenarListasClienteGastos(cliente,gastos):
+        try:
+            for i in range(len(cliente)):
+                for x in range(len(cliente)-1):
+                    if float(gastos[x]) < float(gastos[x+1]):
+                        aux1= float(gastos[x])
+                        gastos[x] = float(gastos[x+1])
+                        gastos[x+1]= aux1
+                        aux2=cliente[x]
+                        cliente[x]=cliente[x+1]
+                        cliente[x+1]=aux2
+            dic = {}
+            dic['cliente'] = cliente
+            dic['gasto'] = gastos
+            return dic
+        except:
+            return False
